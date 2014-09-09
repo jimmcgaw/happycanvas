d6f3472003e5a0737c45a5e376815bcdd3749e5f
Jim McGaw <jpmcgaw@gmail.com>
2014-09-08 21:35:26 -0700

    classes and stuff

--- happyCanvas.js
+++ happyCanvas.js
@@ -1,3 +1,51 @@
+var MAX_BALLS = 100;
+
+var Particle = function(coords, velocity, mass, friction){
+  this.x = coords.x;
+  this.y = coords.y;
+  this.velocity = velocity;
+  this.mass = mass;
+  this.friction = friction;
+
+};
+
+var Ball = function(){
+
+};
+
+var Magnet = function(){
+
+};
+
+var Board = function(elementId){
+  elementId = elementId || 'myCanvas';
+  this.canvas = $(elementId);
+  if (this.canvas.length < 0){
+    console.log('canvas element with id "' + elementId + '" was not found');
+  }
+
+  this._balls = [];
+};
+
+Board.prototype.addBall = function(ball){
+  if (this._balls.length >= MAX_BALLS){
+    console.log('This board is at capacity, not adding ball');
+  }
+  this._balls.push(ball);
+};
+
+Board.prototype.removeBall = function(ball){
+  console.log('removeBall not yet implemented');
+};
+
+Board.prototype.tick = function(){
+
+};
+
+Board.prototype.render = function(){
+
+};
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
